ed0692a2a3f11aac026519f4eaa240cc77f70d13
岡本 悠希 <okachin@okamoto-yuuki-no-MacBook.local>
2012-06-14 17:59:34 +0900

    Rename Array.size() to Array.getSize() & add test

--- package/konoha/array_glue.h
+++ package/konoha/array_glue.h
@@ -53,8 +53,8 @@ static KMETHOD Array_set(CTX, ksfp_t *sfp _RIX)
 	}
 }
 
-//## method int Array.size();
-static KMETHOD Array_size(CTX, ksfp_t *sfp _RIX)
+//## method int Array.getSize();
+static KMETHOD Array_getSize(CTX, ksfp_t *sfp _RIX)
 {
 	kArray *a = sfp[0].a;
 	RETURNi_(kArray_size(a));
@@ -127,7 +127,7 @@ static	kbool_t array_initPackage(CTX, kKonohaSpace *ks, int argc, const char**ar
 	intptr_t MethodData[] = {
 		_Public|_Im, _F(Array_get), TY_T0,   TY_Array, MN_("get"), 1, TY_Int, FN_("index"),
 		_Public,     _F(Array_set), TY_void, TY_Array, MN_("set"), 2, TY_Int, FN_("index"),  TY_T0, FN_("value"),
-		_Public,     _F(Array_size), TY_Int, TY_Array, MN_("size"), 0,
+		_Public,     _F(Array_getSize), TY_Int, TY_Array, MN_("getSize"), 0,
 		_Public,     _F(Array_newArray), TY_Array, TY_Array, MN_("newArray"), 1, TY_Int, FN_("size"),
 		_Public,     _F(Array_add1), TY_void, TY_Array, MN_("add"), 1, TY_T0, FN_("value"),
 		DEND,
